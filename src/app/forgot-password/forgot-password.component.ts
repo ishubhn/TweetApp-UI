0cdd90866610e5bb25484917299ec7fcaf47227f
Shubham Nagre <Shubham.Nagre@Cognizant.com>
2022-09-08 16:14:24 +0530

    Added forgot password logic

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,6 +1,15 @@
 import { NgForm } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { UserService } from '../service/user.service'
 import { faCalendar } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+import { timer } from 'rxjs';
+
+export class UpdateUser {
+    password: String;
+    newPassword: String;
+    dateOfBirth: String;
+}
 
 @Component({
     selector: 'app-forgot-password',
@@ -8,13 +17,75 @@ import { faCalendar } from '@fortawesome/free-solid-svg-icons';
     styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
+    @ViewChild('f', { static: false })
+    updatePasswordForm: NgForm;
     faCalendar = faCalendar;
-    constructor() { }
+    userEmail: String;
+    isUpdatePasswordSuccess: boolean = false;
+    errorMessage: any;
+    subscription: any;
+
+    user: UpdateUser = {
+        password: "",
+        newPassword: "",
+        dateOfBirth: ""
+    }
+
+    constructor(private service: UserService, private router: Router) { }
 
     ngOnInit(): void {
     }
 
-    onSubmit(f: NgForm) {
-        console.log(f);
+    // parse date as per dd/mm/yyyy
+    parseDate = (dateBirth: any) => {
+        let day = dateBirth.day;
+        let month = dateBirth.month;
+        let year = dateBirth.year;
+        let finalDate: String;
+
+        if (month < 10 && day < 10) {
+            finalDate = `0${day}/0${month}/${year}`;
+        } else if (month < 10 && day > 9) {
+            finalDate = `${day}/0${month}/${year}`;
+        } else if (day < 10 && month > 9) {
+            finalDate = `0${day}/${month}/${year}`;
+        } else {
+            finalDate = `${day}/${month}/${year}`;
+        }
+
+        return finalDate;
+    }
+
+    onSubmit = (f: NgForm) => {
+        this.userEmail = this.updatePasswordForm.value.emailId;
+        this.user.password = this.updatePasswordForm.value.password;
+        this.user.newPassword = this.updatePasswordForm.value.newPassword;
+        this.user.dateOfBirth = this.parseDate(this.updatePasswordForm.value.dateOfBirth);
+
+        this.service.forgotPassword(this.userEmail, this.user).subscribe(
+            responseData => {
+                console.log(responseData);
+                console.log("Success")
+                this.isUpdatePasswordSuccess = true;
+                this.wait(3000);
+            },
+            err => {
+                this.errorMessage = err.error.message;
+                console.error("Error");
+                console.error(this.errorMessage);
+            }
+        )
+    }
+
+    wait = (delay: number) => {
+        this.subscription = timer(delay).subscribe(() => {
+            this.router.navigate(['/login']);
+        })
+    };
+
+    ngOnDestroy() {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
     }
 }
