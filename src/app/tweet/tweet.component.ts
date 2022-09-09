d3d7a709e7fe95ebd707d576685365db78540522
Shubham Nagre <Shubham.Nagre@Cognizant.com>
2022-09-09 13:21:52 +0530

    Added Tweet Component

--- src/app/tweet/tweet.component.ts
+++ src/app/tweet/tweet.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faHeart, faRetweet } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
+
 
 @Component({
-  selector: 'app-tweet',
-  templateUrl: './tweet.component.html',
-  styleUrls: ['./tweet.component.css']
+	selector: 'app-tweet',
+	templateUrl: './tweet.component.html',
+	styleUrls: ['./tweet.component.css']
 })
 export class TweetComponent implements OnInit {
+	// Initialization
+	faTwitter = faTwitter;
+	faRetweet = faRetweet;
+	faLike = faHeart;
+	faLikeEmpty = farHeart;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
